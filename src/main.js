94bc9da18c187d1f786ad4a51eff046c6001008f
seanphenomenon <seanvharrell@gmail.com>
2021-01-02 03:03:54 -0800

    added mobile menu overlay

---
+++ src/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  console.log('Opened mobile menu');
+  document.getElementById('myNav').style.height = '100%';
+}
+
+function closeNav() {
+  console.log('Closed mobile menu');
+  document.getElementById('myNav').style.height = '0%';
+}
