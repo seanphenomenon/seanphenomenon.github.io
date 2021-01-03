c3655fa0f3465b1e042e04f2fa3dbea8e38a50bb
seanphenomenon <seanvharrell@gmail.com>
2021-01-02 20:22:29 -0800

    added back to top scroll button to page

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* mobile menu fuctionality */
 function openNav() {
   console.log('Opened mobile menu');
   document.getElementById('myNav').style.height = '100%';
@@ -6,4 +7,21 @@ function openNav() {
 function closeNav() {
   console.log('Closed mobile menu');
   document.getElementById('myNav').style.height = '0%';
+}
+
+/* button scroll functionality */
+const btnScrollToTop = document.querySelector('#btnScrollToTop');
+
+window.addEventListener('scroll', scrollFunction);
+
+btnScrollToTop.addEventListener('click', function () {
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+});
+
+function scrollFunction() {
+  if (window.pageYOffset > 300) {
+    btnScrollToTop.style.display = 'block';
+  } else {
+    btnScrollToTop.style.display = 'none';
+  }
 }
